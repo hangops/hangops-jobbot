6410983d865ef75a6520a2d28771507cb892faf1
Gabe Abinante <gabinante@clearslide.com>
2017-03-10 14:10:15 -0800

    simplify if

--- cookbooks/hangops-jobbot/recipes/default.rb
+++ cookbooks/hangops-jobbot/recipes/default.rb
@@ -81,8 +81,6 @@ service_provider = nil
 if 'ubuntu' == node['platform']
   if Chef::VersionConstraint.new('>= 15.04').include?(node['platform_version'])
     service_provider = Chef::Provider::Service::Systemd
-  elsif Chef::VersionConstraint.new('>= 12.04').include?(node['platform_version'])
-    service_provider = Chef::Provider::Service::Upstart
   end
 end
 
